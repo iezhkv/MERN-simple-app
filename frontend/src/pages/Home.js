6bce3cd47070990a9241e8e001ebf853b8f8da0c
iezhkv <lev3el6up9@gmail.com>
2023-08-23 23:28:50 +0300

    Added a create form

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,36 @@
+import { useEffect , useState } from "react"
+
+//  components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutsForm from "../components/WorkoutsForm"
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if(response.ok) {
+                setWorkouts(json.workouts)
+            }
+        }
+
+        fetchWorkouts()
+
+    }, [])
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
+            <WorkoutsForm />
         </div>
     )
 }
