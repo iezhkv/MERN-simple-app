88025721528246118f7649987338bc7a74df011c
iezhkv <lev3el6up9@gmail.com>
2023-08-26 19:46:20 +0300

    Auth on the backend
    Created routes , controller and a model for the backend

---
+++ backend/models/userModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password: {
+        type:String,
+        required: true
+    }
+});
+
+//  static signup method
+userSchema.statics.signup = async function(email, password) {
+
+    // validation
+    if(!email || !password) {
+        throw Error('All fields are required');
+    }
+    if(!validator.isEmail(email)) {
+        throw Error('Invalid email');
+    }
+    if(!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough');
+    }
+
+    const exist = await this.findOne({email});
+
+    if(exist) {
+        throw Error('User already exists');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({email, password: hash});
+
+    return user;
+
+}
+
+
+
+module.exports = mongoose.model('User', userSchema)
+
