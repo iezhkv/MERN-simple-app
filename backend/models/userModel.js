c8970f2b8774e5670810fc1570d881953a86d247
iezhkv <lev3el6up9@gmail.com>
2023-08-26 22:45:38 +0300

    JWT added
    Created login function and added json web tokens

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,6 +16,32 @@ const userSchema = new Schema({
     }
 });
 
+//  static login method
+userSchema.statics.login = async function(email, password) {
+
+    // validation
+    if(!email || !password) {
+        throw Error('All fields are required');
+    }
+
+    const user = await this.findOne({email})
+
+
+    if(!user) {
+        throw Error('User does not exist');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    
+
+    if(!match) {
+        throw Error('Incorrect password');
+    }
+
+    return user;
+
+}
+
 //  static signup method
 userSchema.statics.signup = async function(email, password) {
 
