4b494578b1d7c5da7465d5e51a04b796cfc32075
iezhkv <lev3el6up9@gmail.com>
2023-08-27 02:14:57 +0300

    isAuth from local storage

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,15 @@
 import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 
 const Login = () => { 
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { login, error, isLoading } = useLogin()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log('username', email)
-        console.log('password', password)
+        login(email, password)
     }
 
     return (
@@ -30,7 +31,9 @@ const Login = () => {
             value={password}
         />   
 
-        <button>Log in</button>
+        <button disabled={isLoading}>Log in</button>
+        {error && <div className="error">{error}</div>}
+
 
        </form>
     )
