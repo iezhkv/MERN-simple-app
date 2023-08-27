a98112c5c08236794f9836b6be12b074a593a95d
iezhkv <lev3el6up9@gmail.com>
2023-08-27 20:40:39 +0300

    Assigning workouts to creator

--- backend/middlewares/requireAuth.js
+++ backend/middlewares/requireAuth.js
@@ -18,7 +18,7 @@ const requireAuth = async (req, res, next) => {
     try {
        const {_id} = jwt.verify(token, process.env.SECRET)
 
-       req.user = User.findOne({_id}).select('_id')
+       req.user = await User.findOne({_id}).select('_id')
 
        next();
 
