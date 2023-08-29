02be4c89f45dd1e3a224fb2e066a18eaada7cf83
iezhkv <lev3el6up9@gmail.com>
2023-08-29 03:18:35 +0300

    Docker Compose
    after 100+ unsuccessful containers , finally managed to make it work

--- backend/server.js
+++ backend/server.js
@@ -13,32 +13,25 @@ const userRoutes = require('./routes/user');
 //express app
 const app = express();
 
-// Serve static files from the frontend build
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
+
 
 //routes
 app.use('/api/workouts', workoutsRoutes);
 app.use('/api/user', userRoutes);
 
-// Serve the frontend build for all other routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-  });
+
 
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log('Connected to db & listening for requests on port ' + process.env.PORT)
+        app.listen(4000, () => {
+            console.log('Connected to db & listening for requests on port 4000')
         });
     })
     .catch((error) => {
